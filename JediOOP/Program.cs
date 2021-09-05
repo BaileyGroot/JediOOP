c9170ed224f2c63f6a08908cc75d45cc50823ac0
Markus Bailey Stockfleth Thomsbæk Groot <mark4805@zbc.dk>
2021-09-05 16:02:32 +0200

    Add project files.

---
+++ JediOOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JediOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Dictionary<string, int> Task = new Dictionary<string, int>();
+
+            // Adding name and age to the dictionary.
+            Task.Add("Bailey", 20);
+
+            // Adding a new name to the value.
+            Task["Han"] = new int();
+
+            // It removes the "key" from the dictionary but if it couldn't find the key it will return with invalid response.
+
+            if (Task.Remove("Han"))
+            {
+                Console.WriteLine("Removed from the dictionary");
+            }
+            else
+            {
+                Console.WriteLine("Couldn't locate the key.");
+            }
+
+            // How to add "age" to a value when it 
+
+            foreach (var emp in Task)
+            {
+                Console.WriteLine(emp.Key);
+            }
+
+
+        }
+    }
+}
+ 
